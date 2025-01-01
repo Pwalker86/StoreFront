638cd0bf39c45b23822976fd1bcb489a7426c974
Phil Walker <phil1586@gmail.com>
2025-01-01 11:37:32 -0700

    very rough implementation of a responsive menu.
    Keeping for reference for now

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menuItems"];
+  static targets = ["offScreen", "menuTrigger"];
 
-  toggleMenu() {}
+  toggleMenu() {
+    this.menuTriggerTarget.classList.toggle("fa-x");
+    this.menuTriggerTarget.classList.toggle("fa-bars");
+    this.offScreenTarget.classList.toggle("open");
+  }
 }
