ec883a7116ec605307e572c9dcfc472d15328dca
Phil Walker <phil1586@gmail.com>
2025-01-01 11:36:59 -0700

    better styles and scsss organization

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = ["reviewForm", "reviewFormTrigger"];
 
   toggleReviewForm() {
-    debugger;
     if (this.reviewFormTarget.classList.contains("hidden")) {
       this.reviewFormTriggerTarget.innerText = "Close Form";
       this.reviewFormTarget.classList.remove("hidden");
