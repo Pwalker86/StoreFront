f3c51606e623946942bb737d04d26fb9b8c0a56d
Phil Walker <phil1586@gmail.com>
2025-01-07 16:23:34 -0700

    minor styling for store reveiws

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,4 @@ class Review < ApplicationRecord
   scope :ordered, -> { order(created_at: :desc) }
 
   # after_create_commit -> { broadcast_prepend_to "store_reviews", partial: "store_reviews/review", locals: { review: self }, target: "store_reviews" }
-
 end
