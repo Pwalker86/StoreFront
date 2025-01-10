1529aae7d50fe626e1e0b46f72e4f113fe469411
Phil Walker <phil1586@gmail.com>
2025-01-10 12:55:21 -0700

    security
    create cart update service
     - use a hash to interpret parameter safely
    
    re-add price method for cart model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,4 @@
+# TODO: This needs to be moved over to a new ProductReviewsController like has already been done with StoreReviewsController
 class ReviewsController < ApplicationController
   def index
   end
@@ -6,22 +7,22 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @reviewable = params[:reviewable_class].constantize.find(params[:reviewable_id])
+    # @reviewable = params[:reviewable_class].constantize.find(params[:reviewable_id])
   end
 
   def create
-    reviewable = review_find_params[:reviewable_class].constantize.find(review_find_params[:reviewable_id])
-    review = reviewable.reviews.new(review_params)
-    respond_to do |format|
-      path = reviewable.is_a?(Product) ? store_product_path([ reviewable.store, reviewable ]) : reviewable
-      if review.save
-        format.html { redirect_to path, notice: "Your review has been created!" }
-        format.turbo_stream
-      else
-        format.html { redirect_to path, alert: "Something went wrong with your review" }
-        format.json { render json: review.errors, status: :unprocessable_entity }
-      end
-    end
+    # reviewable = review_find_params[:reviewable_class].constantize.find(review_find_params[:reviewable_id])
+    # review = reviewable.reviews.new(review_params)
+    # respond_to do |format|
+    #   path = reviewable.is_a?(Product) ? store_product_path([ reviewable.store, reviewable ]) : reviewable
+    #   if review.save
+    #     format.html { redirect_to path, notice: "Your review has been created!" }
+    #     format.turbo_stream
+    #   else
+    #     format.html { redirect_to path, alert: "Something went wrong with your review" }
+    #     format.json { render json: review.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   def edit
---
+++ app/services/cart_update_service.rb
@@ -0,0 +1,30 @@
+class CartUpdateService
+  def initialize(user_entity_param, user_id, product_id, quantity)
+    safe_constants = {
+      "Guest": Guest,
+      "User": User
+    }
+    @user_entity_class = safe_constants[user_entity_param.to_sym]
+    @user_id = user_id
+    @product_id = product_id
+    @quantity = quantity
+  end
+
+  def call
+    debugger
+    cart = @user_entity_class.find(@user_id).cart
+    product = Product.find(@product_id)
+
+    cart_item = cart.cart_items.find_or_initialize_by(product: product)
+    cart_item.increment(:quantity, @quantity.to_i)
+
+    if cart_item.quantity <= 0
+      cart_item.destroy
+      { status: :removed, message: "Product was successfully removed from your order." }
+    elsif cart_item.save
+      { status: :updated, message: "Quantity was successfully updated." }
+    else
+      { status: :error, message: "There was an error updating the product quantity." }
+    end
+  end
+end
