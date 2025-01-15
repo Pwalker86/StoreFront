d5f57d7e04df83ba82d49f90b879ebad65b02e82
Phil Walker <phil1586@gmail.com>
2025-01-15 14:50:54 -0700

    refactor search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
-class ProductSearchController < ApplicationController
-  def home_search
+class SearchController < ApplicationController
+  def product_search
     @products = Product.search_by(params[:query]).select(:id, :store_id, :name).map do |item|
       { href: "/stores/#{item.store_id}/products/#{item.id}", name: item.name }
     end
     respond_to do |format|
-      format.html {  }
+      format.html { }
       format.json { render json: @products }
     end
   end
