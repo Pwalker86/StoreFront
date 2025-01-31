f589514f1a0371f7afb5c4e8bde0148e20bea180
Phil Walker <phil1586@gmail.com>
2025-01-31 00:04:54 -0700

    make search controller RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,8 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   get "products", to: "pages#products"
-  get "product_search", to: "search#product_search", as: "search"
+  resources :search, only: :index
+  # get "product_search", to: "search#product_search", as: "search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
