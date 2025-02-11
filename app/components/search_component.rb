8048d935d2b5051d5149bb05f2f6c9ce65a77283
Phil Walker <phil1586@gmail.com>
2025-02-11 14:01:16 -0700

    add tests

--- app/components/search_component.rb
+++ app/components/search_component.rb
@@ -2,6 +2,7 @@
 
 class SearchComponent < ViewComponent::Base
   def initialize(url:, input_placeholder: "Search...")
+    raise ArgumentError, "url is required" unless url
     @url = url
     @input_placeholder = input_placeholder
   end
--- test/components/search_component_test.rb
+++ test/components/search_component_test.rb
@@ -8,4 +8,17 @@ class SearchComponentTest < ViewComponent::TestCase
     assert_component_rendered
     assert_selector "form[action='/home_search']"
   end
+
+  def test_raise_no_url_error
+    assert_raises ArgumentError do
+      render_inline(SearchComponent.new(url: nil)).to_html
+    end
+  end
+
+  def test_renders_with_custom_placeholder
+    placeholder = "What do you want?"
+    render_inline(SearchComponent.new(url: "/home_search", input_placeholder: placeholder)).to_html
+    assert_component_rendered
+    assert_selector "input[placeholder='#{placeholder}']"
+  end
 end
