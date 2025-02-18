36d59b4f102c2687923f78adc5bfebe92304eddc
Phil Walker <phil1586@gmail.com>
2025-02-18 10:59:55 -0700

    StoreImages for non-admins

--- app/controllers/store_images_controller.rb
+++ app/controllers/store_images_controller.rb
@@ -1,5 +1,10 @@
 class StoreImagesController < ApplicationController
   def show
+    begin
+      @store = Store.find(params[:store_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: "Store not found!" and return
+    end
     @spotlight = Store.find(params[:store_id]).spotlight
   end
 end
