d7b91eed2934d12d78312d2a62a5091d3ddfe9e4
Phil Walker <phil1586@gmail.com>
2025-02-21 16:08:59 -0700

    update store model test

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -28,14 +28,14 @@ class StoreTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @store.name = "   "
+    @store.name = nil
     assert_not @store.valid?
     @store.name = "Store1"
     assert @store.valid?
   end
 
   test "email should be present" do
-    @store.email=nil
+    @store.email= nil
     assert_not @store.valid?
     @store.email = "store1@email.com"
     assert @store.valid?
@@ -47,31 +47,16 @@ class StoreTest < ActiveSupport::TestCase
   end
 
   test "location should be present" do
-    @store.location=nil
+    @store.location = nil
     assert_not @store.valid?
     @store.location = "Utah, US"
     assert @store.valid?
   end
 
   test "should have a phone number" do
-    @store.phone_number=nil
+    @store.phone_number = nil
     assert_not @store.valid?
     @store.phone_number = "8015551234"
     assert @store.valid?
   end
-
-  test "should have a store admin" do
-    assert_equal store_admins(:store_admin_one), @store.store_admin
-  end
-
-  test "should have a review" do
-    assert_equal @store.reviews.first, reviews(:store_review)
-  end
-
-  # NOTE: Not needed as I've removed the after save callback for now. Instead will implement a prompt notifying the user that their store cannot be featured if they don't attached a photo
-  # test "a spotlight is attached if there's not already one" do
-  #   assert_not @store.spotlight.attached?
-  #   @store.save
-  #   assert @store.spotlight.attached?
-  # end
 end
