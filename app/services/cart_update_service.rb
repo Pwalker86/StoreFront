3ee3b340f97c31e577f2ca6ea4db681f64c8c541
Phil Walker <phil1586@gmail.com>
2025-03-03 16:25:26 -0700

    carts controller tests

--- app/services/cart_update_service.rb
+++ app/services/cart_update_service.rb
@@ -6,18 +6,20 @@ class CartUpdateService
   end
 
   def call
-    cart = @user_entity.cart
-    product = Product.find(@product_id)
+    begin
+      cart = @user_entity.cart
+      product = Product.find(@product_id)
 
-    cart_item = cart.cart_items.find_or_initialize_by(product: product)
-    cart_item.increment(:quantity, @quantity.to_i)
+      cart_item = cart.cart_items.find_or_initialize_by(product: product)
+      cart_item.increment(:quantity, @quantity.to_i)
 
-    if cart_item.quantity <= 0
-      cart_item.destroy
-      { status: :removed, message: "Product was successfully removed from your order." }
-    elsif cart_item.save
-      { status: :updated, message: "Quantity was successfully updated." }
-    else
+      if cart_item.quantity <= 0
+        cart_item.destroy
+        { status: :removed, message: "Product was successfully removed from your order." }
+      elsif cart_item.save
+        { status: :updated, message: "Quantity was successfully updated." }
+      end
+    rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved
       { status: :error, message: "There was an error updating the product quantity." }
     end
   end
