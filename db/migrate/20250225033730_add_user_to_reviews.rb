f7d2f61a7ea23eb15c8212b9ea73ce0cd0c9c8ca
Phil Walker <phil1586@gmail.com>
2025-03-05 15:28:39 -0700

    add rake task to migration to populate new foreign key

--- db/migrate/20250225033730_add_user_to_reviews.rb
+++ db/migrate/20250225033730_add_user_to_reviews.rb
@@ -1,5 +1,15 @@
+require 'rake'
+Rails.application.load_tasks
+
 class AddUserToReviews < ActiveRecord::Migration[8.0]
   def change
     add_reference :reviews, :user, foreign_key: true
+
+    # Invoke the rake task after the schema change
+    reversible do |dir|
+      dir.up do
+        Rake::Task['reviews:update_user_id'].invoke
+      end
+    end
   end
 end
