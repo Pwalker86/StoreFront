74cd57e75452a65dc08dd4ef098917c9eeb817a3
Phil Walker <phil1586@gmail.com>
2025-03-12 07:52:56 -0600

    pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @stores = Store.joins(:spotlight_attachment).where.not(spotlight_attachment: { id: nil }).limit(12)
+    @stores = Store.joins(:spotlight_attachment).where.not(spotlight_attachment: { id: nil }).limit(6)
   end
 end
