f1ab896b0aeccf9210d5746c311ee686307b2728
Phil Walker <phil1586@gmail.com>
2025-04-02 10:40:25 -0600

    create order export with the selected orders
    Can improve to handle date ranges, current days orders, etc

--- app/controllers/order_exports_controller.rb
+++ app/controllers/order_exports_controller.rb
@@ -6,10 +6,17 @@ class OrderExportsController < ApplicationController
   end
 
   def create
-    debugger
+    orders = Order.where(id: order_export_params[:order_ids])
     @order_export = OrderExport.new
     @order_export.date_exported_at = Time.current
+    @order_export.orders = orders
     @order_export.save
-    redirect_to order_exports_path, notice: "Order export created!"
+    redirect_to store_admin_store_orders_path(current_store_admin, current_store_admin.store), notice: "Order export created!"
+  end
+
+  private
+
+  def order_export_params
+    params.expect(order_export: { order_ids: [] })
   end
 end
