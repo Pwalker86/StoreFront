89897a6ff5067291a6cb1df181c7056c8c2dc565
Phil Walker <phil1586@gmail.com>
2025-04-14 12:54:31 -0600

    fix tests

--- test/services/active_user_service_test.rb
+++ test/services/active_user_service_test.rb
@@ -5,23 +5,23 @@ class ActiveUserServiceTest < ActiveSupport::TestCase
     @session = {}
     @current_user = nil
     @current_store_admin = nil
-    @service = ActiveUserService.new(@session, @current_user, @current_store_admin)
   end
 
   test "returns current_user when current_user is present" do
     @current_user = users(:user_one)
-    @service = ActiveUserService.new(@session, @current_user, @current_store_admin)
+    @service = ActiveUserService.new(session: @session, current_user: @current_user, current_store_admin: @current_store_admin)
     assert_equal @current_user, @service.call
   end
 
   test "returns guest when current_user is nil and session[:guest_id] is present" do
     @session[:guest_id] = guests(:guest_one).id
-    @service = ActiveUserService.new(@session, @current_user, @current_store_admin)
+    @service = ActiveUserService.new(session: @session, current_user: @current_user, current_store_admin: @current_store_admin)
     assert_equal guests(:guest_one), @service.call
   end
 
   test "returns guest when current_user is nil and session[:guest_id] is nil" do
     assert_difference "Guest.count", 1 do
+      @service = ActiveUserService.new(session: @session, current_user: @current_user, current_store_admin: @current_store_admin)
       @service.call
       assert_not_nil @session[:guest_id]
     end
