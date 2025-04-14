525a71745e6f28dfc46cf8e3edeb48c28b1489e2
Phil Walker <phil1586@gmail.com>
2025-04-14 00:10:12 -0600

    redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.credentials.dig(:redis_url) }
+  config.redis = { url: Rails.application.credentials.dig(:redis_url), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.credentials.dig(:redis_url) }
+  config.redis = { url: Rails.application.credentials.dig(:redis_url), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
