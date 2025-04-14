d17f5cb7f0d2a8cfa7986abb4c46c857821ca0ab
Phil Walker <phil1586@gmail.com>
2025-04-14 00:21:32 -0600

    fix some side menu issues

--- app/helpers/user_navigation_helper.rb
+++ app/helpers/user_navigation_helper.rb
@@ -10,6 +10,22 @@ module UserNavigationHelper
     if current_user && !current_user.is_a?(Guest)
       links << { name: "My Orders", path: orders_path }
       links << { name: "Log Out", path: destroy_user_session_path, method: :delete }
+    end
+
+    links
+  end
+
+  def side_nav_links
+    links = []
+
+    # Add links available to all users
+    links << { name: "Home", path: root_path }
+    links << { name: "Browse Products", path: root_path }
+
+    # Add links for logged-in users
+    if current_user && !current_user.is_a?(Guest)
+      links << { name: "My Orders", path: orders_path }
+      links << { name: "Log Out", path: destroy_user_session_path, method: :delete }
     else
       links << { name: "Sign In As User", path: new_user_session_path }
       links << { name: "Sign Up As User", path: new_user_registration_path }
