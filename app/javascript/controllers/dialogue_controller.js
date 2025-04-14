39d07aa08f9d27f34fa842be1b173646ca249d7f
Phil Walker <phil1586@gmail.com>
2025-04-13 22:00:03 -0600

    auto open option for dialogue

--- app/javascript/controllers/dialogue_controller.js
+++ app/javascript/controllers/dialogue_controller.js
@@ -2,14 +2,29 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["container", "panel", "trigger", "overlay"];
+  static values = {
+    cookieName: String,
+    autoOpen: Boolean
+  }
 
   connect() {
     // Set up escape key listener
     this.escapeKeyHandler = this.handleEscapeKey.bind(this);
     document.addEventListener("keydown", this.escapeKeyHandler);
     
-    // Set up click event on trigger
-    this.triggerTarget.addEventListener("click", this.open.bind(this));
+    // Set up click event on trigger if it exists
+    if (this.hasTriggerTarget) {
+      this.triggerTarget.addEventListener("click", this.open.bind(this));
+    }
+    
+    // Check if we should auto-open
+    if (this.autoOpenValue && this.hasCookieNameValue) {
+      // Only auto-open if the cookie doesn't exist
+      if (!this.getCookie(this.cookieNameValue)) {
+        // Small delay to ensure DOM is fully loaded
+        setTimeout(() => this.open(), 100);
+      }
+    }
   }
 
   disconnect() {
@@ -35,6 +50,11 @@ export default class extends Controller {
   }
 
   close() {
+    // Set cookie if we have a cookie name
+    if (this.hasCookieNameValue) {
+      this.setCookie(this.cookieNameValue, "dismissed", 365);
+    }
+    
     this.containerTarget.classList.add("hidden");
     document.body.classList.remove("overflow-hidden");
   }
@@ -54,5 +74,29 @@ export default class extends Controller {
     if (focusableElements.length > 0) {
       focusableElements[0].focus();
     }
+  }
+  
+  // Cookie helpers
+  setCookie(name, value, days) {
+    const date = new Date();
+    if (days === Infinity || days === undefined) {
+      // Set to a very distant future date (Jan 1, 2099)
+      date.setFullYear(2099, 0, 1);
+    } else {
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    }
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Lax";
+  }
+  
+  getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
   }
 }
