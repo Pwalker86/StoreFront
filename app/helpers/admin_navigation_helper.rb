25120729bf6e96548ddeba6820b903c4050551ca
Phil Walker <phil1586@gmail.com>
2025-04-14 00:27:17 -0600

    admin nav links

--- app/helpers/admin_navigation_helper.rb
+++ app/helpers/admin_navigation_helper.rb
@@ -3,27 +3,27 @@ module AdminNavigationHelper
     return [] unless current_store_admin
 
     links = []
-    links << {name: "Import Shipping Details", path: new_store_admin_shipping_detail_path(current_store_admin)}
+    links << { name: "Import Shipping Details", path: new_store_admin_shipping_detail_path(current_store_admin) }
 
     if store_exists?
-      links << {name: "View Store", path: store_admin_store_path(current_store_admin, current_store_admin.store)}
-      links << {name: "Edit Store", path: edit_store_admin_store_path(current_store_admin, current_store_admin.store)}
-      links << {name: "View All Orders", path: store_admin_store_orders_path(current_store_admin, current_store_admin.store)}
+      links << { name: "View Store", path: store_admin_store_path(current_store_admin, current_store_admin.store) }
+      links << { name: "Edit Store", path: edit_store_admin_store_path(current_store_admin, current_store_admin.store) }
+      links << { name: "View All Orders", path: store_admin_store_orders_path(current_store_admin, current_store_admin.store) }
 
       # Fulfillment partner links
       if current_store_admin.store.fulfillment_partner.present?
-        links << {name: "Fulfillment Partners", path: store_admin_store_fulfillment_partner_path(
-          current_store_admin, current_store_admin.store, current_store_admin.store.fulfillment_partner)}
+        links << { name: "Fulfillment Partners", path: store_admin_store_fulfillment_partner_path(
+          current_store_admin, current_store_admin.store, current_store_admin.store.fulfillment_partner) }
       else
-        links << {name: "Create Fulfillment Partner", path: new_store_admin_store_fulfillment_partner_path(
-          current_store_admin, current_store_admin.store)}
+        links << { name: "Create Fulfillment Partner", path: new_store_admin_store_fulfillment_partner_path(
+          current_store_admin, current_store_admin.store) }
       end
     else
-      links << {name: "Create Store", path: new_store_admin_store_path(current_store_admin)}
+      links << { name: "Create Store", path: new_store_admin_store_path(current_store_admin) }
     end
 
 
-    links << {name: "Log Out", path: destroy_store_admin_session_path, method: :delete}
+    links << { name: "Log Out", path: destroy_store_admin_session_path, method: :delete }
     links
   end
 
