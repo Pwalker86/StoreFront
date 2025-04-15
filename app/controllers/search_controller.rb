768c693094702e07833096e4a588b67e5e833caa
Phil Walker <phil1586@gmail.com>
2025-04-15 16:08:45 -0600

    format AR query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,14 @@
 class SearchController < ApplicationController
   def index
     # limit to 11 because search_controller.js is limiting at 10 and adding a 'See More' link.
-    @products = Product.search_by(params[:query]).joins(:store).limit(11).select('products.id', 'products.store_id', 'products.name', 'stores.name as store_name').to_a.map do |item|
+    @products = Product
+    .search_by(params[:query])
+    .joins(:store)
+    .where(archived: false)
+    .limit(11)
+    .select("products.id", "products.store_id", "products.name", "stores.name as store_name")
+    .to_a
+    .map do |item|
       { href: "/stores/#{item.store_id}/products/#{item.id}", name: item.name, store_name: item.store_name }
     end
     respond_to do |format|
